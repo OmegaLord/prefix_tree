2084e25ddaab6f2453e32ee0e58e97509bf719b2
OmegaLord <omega32150@gmail.com>
2019-06-25 23:19:16 +0300

    Add include? and test

--- prefix_tree_spec.rb
+++ prefix_tree_spec.rb
@@ -9,6 +9,7 @@ class TestPrefixTree < Minitest::Test
   def setup
     @tree = Tree.new
     @str = 'Lorem'
+    @tree.add(@str.split)
   end
 
   # test_new_tree for test creation new class Tree object
@@ -18,9 +19,12 @@ class TestPrefixTree < Minitest::Test
 
   # test_add_function for check add words
   def test_add_function
-    assert @tree.add(@str.split)
-    pointer_node = @tree.root.elements
-    @str.each_char do |c|
+    find_each_char_in_tree(@str, @tree)
+  end
+
+  def find_each_char_in_tree(string, tree)
+    pointer_node = tree.root.child_arr
+    string.each_char do |c|
       pointer_node = try_find_node_with_char(pointer_node, c)
       assert_equal c, pointer_node.character
       pointer_node = get_deep_node(pointer_node)
@@ -29,11 +33,17 @@ class TestPrefixTree < Minitest::Test
 
   # function get_deep_node gets node which are located deer in tree
   def get_deep_node(node)
-    node.elements
+    node.child_arr
   end
 
   # function try_find_node_with_char found char in node
   def try_find_node_with_char(node, char)
     node.find { |n| n.character == char }
+  end
+
+  # test_include_function for check word in tree
+  def test_include_function
+    assert @tree.include?(@str)
+    find_each_char_in_tree(@str, @tree)
   end
 end
